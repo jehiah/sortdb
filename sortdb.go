b9cf59ef3193365136b97080ebf331d89f909efb
Jehiah Czebotar <jehiah@gmail.com>
2014-12-22 23:42:23 -0500

    working binary search

--- sortdb.go
+++ sortdb.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"log"
 	"os"
+	"sort"
 
 	"github.com/riobard/go-mmap"
 )
@@ -11,7 +12,7 @@ import (
 type DB struct {
 	f    *os.File
 	data mmap.Mmap
-	size int64
+	size int
 }
 
 func OpenDB(f *os.File) (*DB, error) {
@@ -19,8 +20,8 @@ func OpenDB(f *os.File) (*DB, error) {
 	if err != nil {
 		return nil, err
 	}
-	size := fi.Size()
-	data, err := mmap.Map(f, 0, int(size), mmap.PROT_READ, mmap.MAP_FILE|mmap.MAP_SHARED)
+	size := int(fi.Size())
+	data, err := mmap.Map(f, 0, size, mmap.PROT_READ, mmap.MAP_FILE|mmap.MAP_SHARED)
 	db := &DB{
 		f:    f,
 		data: data,
@@ -34,6 +35,60 @@ func (db *DB) Close() {
 	db.f = nil
 }
 
+// LastIndexByte returns the index of the first instance of c in s, or -1 if c is not present in s after start.
+func LastIndexByte(s []byte, i int, c byte) int {
+	for ; i >= 0; i-- {
+		if s[i] == c {
+			return i
+		}
+	}
+	return -1
+}
+
+// IndexByte returns the index of the first instance of c in s after i but before m. If c is not present in s -1 is returned
+func IndexByte(s []byte, i, m int, c byte) int {
+	for ; i < m; i++ {
+		if s[i] == c {
+			return i
+		}
+	}
+	return -1
+}
+
+// Search uses a binary search looking for needle based
+func (db *DB) Search(needle []byte, newline, recordSep byte) ([]byte, bool) {
+	// Binary search to find a matching byte slice.
+	needle = append(needle, recordSep)
+
+	needleLen := len(needle)
+	i := sort.Search(db.size, func(i int) bool {
+		// find previous line starting point
+		previous := LastIndexByte(db.data, i, newline)
+		if previous == -1 {
+			return false
+		}
+		// make sure we have space before end of the buffer
+		if previous+1+needleLen > db.size {
+			return false
+		}
+		return bytes.Compare(db.data[previous+1:previous+1+needleLen], needle) >= 0
+	})
+	if i < 0 {
+		return nil, false
+	}
+	previous := LastIndexByte(db.data, i, newline)
+	lineEnd := IndexByte(db.data, previous+1, db.size, newline)
+	return []byte(db.data[previous+1+needleLen : lineEnd]), true
+
+	// re-check equals?
+	// if i+needleLen < db.size && bytes.Equal(db.data[i:i+needleLen], needle) {
+	// 	previous := LastIndexByte(db.data, i, '\n')
+	// 	lineEnd := IndexByte(db.data, i, db.size, '\n')
+	// 	return []byte(db.data[i:lineEnd]), true
+	// }
+	// return nil, false
+}
+
 func main() {
 	f, err := os.Open("test_data/testdb.tab")
 	if err != nil {
@@ -43,8 +98,10 @@ func main() {
 	if err != nil {
 		log.Fatalf("error mapping %s", err)
 	}
-	end := bytes.Index(db.data, []byte("\n"))
-	record := []byte(db.data[:end])
-	log.Printf("record is %q", record)
+
+	for _, q := range []string{"prefix.1", "e", "a", "aa", "zzzzzzzzzzzzzzzzzzzzzzzzzz"} {
+		f, ok := db.Search([]byte(q), '\n', '\t')
+		log.Printf("search %q found %q %v", q, f, ok)
+	}
 	db.Close()
 }
---
+++ sortdb_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"testing"
+)
+
+type testCase struct {
+	startIndex int
+	needle     byte
+	expected   int
+}
+
+func TestLastIndexByte(t *testing.T) {
+	b := []byte{'a', 'b', 'c', 'd', 'e', 'a', 'b', 'c', 'd', 'e'}
+	for _, tc := range []testCase{
+		testCase{3, 'a', 0},
+		testCase{5, 'b', 1},
+		testCase{3, 'e', -1},
+	} {
+		i := LastIndexByte(b, tc.startIndex, tc.needle)
+		if i != tc.expected {
+			t.Errorf("got %d expected %d searching for %q in %q", i, tc.expected, tc.needle, b)
+		}
+	}
+}
