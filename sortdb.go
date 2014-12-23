87ed5f8a2c0af6639bad454ee6d536788d47dbbc
Jehiah Czebotar <jehiah@gmail.com>
2014-12-22 22:47:59 -0500

    refactor into db class

--- sortdb.go
+++ sortdb.go
@@ -8,13 +8,30 @@ import (
 	"github.com/riobard/go-mmap"
 )
 
-func Map(f *os.File) (mmap.Mmap, error) {
+type DB struct {
+	f    *os.File
+	data mmap.Mmap
+	size int64
+}
+
+func OpenDB(f *os.File) (*DB, error) {
 	fi, err := f.Stat()
 	if err != nil {
 		return nil, err
 	}
-	m, err := mmap.Map(f, 0, int(fi.Size()), mmap.PROT_READ, mmap.MAP_FILE|mmap.MAP_SHARED)
-	return m, err
+	size := fi.Size()
+	data, err := mmap.Map(f, 0, int(size), mmap.PROT_READ, mmap.MAP_FILE|mmap.MAP_SHARED)
+	db := &DB{
+		f:    f,
+		data: data,
+		size: size,
+	}
+	return db, err
+}
+func (db *DB) Close() {
+	db.data.Unmap()
+	db.f.Close()
+	db.f = nil
 }
 
 func main() {
@@ -22,11 +39,12 @@ func main() {
 	if err != nil {
 		log.Fatalf("error opening %s", err)
 	}
-	data, err := Map(f)
+	db, err := OpenDB(f)
 	if err != nil {
 		log.Fatalf("error mapping %s", err)
 	}
-	end := bytes.Index(data, []byte("\n"))
-	record := []byte(data[:end])
+	end := bytes.Index(db.data, []byte("\n"))
+	record := []byte(db.data[:end])
 	log.Printf("record is %q", record)
+	db.Close()
 }
