21c259c0ed5d57f2b30736b1ef4392c5ef947fe8
Jehiah Czebotar <jehiah@gmail.com>
2014-12-23 23:04:40 -0500

    finish stats endpoint with metrics

--- http.go
+++ http.go
@@ -7,6 +7,9 @@ import (
 	"net/http"
 	"strconv"
 	"sync/atomic"
+	"time"
+
+	"github.com/jehiah/sortdb/timer_metrics"
 )
 
 type httpServer struct {
@@ -19,11 +22,16 @@ type httpServer struct {
 	MgetRequests uint64
 	MgetHits     uint64
 	MgetMisses   uint64
+
+	GetMetrics  *timer_metrics.TimerMetrics
+	MgetMetrics *timer_metrics.TimerMetrics
 }
 
 func NewHTTPServer(ctx *Context) *httpServer {
 	return &httpServer{
-		ctx: ctx,
+		ctx:         ctx,
+		GetMetrics:  timer_metrics.NewTimerMetrics(1500, "/get"),
+		MgetMetrics: timer_metrics.NewTimerMetrics(1500, "/mget"),
 	}
 }
 
@@ -60,21 +68,24 @@ func (s *httpServer) getHandler(w http.ResponseWriter, req *http.Request) {
 		http.Error(w, "MISSING_ARG_KEY", 400)
 		return
 	}
+	startTime := time.Now()
 	atomic.AddUint64(&s.Requests, 1)
 	atomic.AddUint64(&s.GetRequests, 1)
+
 	needle := append([]byte(key), s.ctx.db.RecordSeparator)
 	line := s.ctx.db.Search(needle)
 
 	if len(line) == 0 {
 		atomic.AddUint64(&s.GetMisses, 1)
 		http.Error(w, "NOT_FOUND", 404)
-		return
+	} else {
+		atomic.AddUint64(&s.GetHits, 1)
+		w.Header().Set("Content-Type", "text/plain")
+		w.Header().Set("Content-Length", strconv.Itoa(len(line)+1))
+		w.Write(line)
+		w.Write([]byte{s.ctx.db.LineEnding})
 	}
-	atomic.AddUint64(&s.GetHits, 1)
-	w.Header().Set("Content-Type", "text/plain")
-	w.Header().Set("Content-Length", strconv.Itoa(len(line)+1))
-	w.Write(line)
-	w.Write([]byte{s.ctx.db.LineEnding})
+	s.GetMetrics.Status(startTime)
 }
 
 func (s *httpServer) mgetHandler(w http.ResponseWriter, req *http.Request) {
@@ -83,6 +94,7 @@ func (s *httpServer) mgetHandler(w http.ResponseWriter, req *http.Request) {
 		http.Error(w, "MISSING_ARG_KEY", 400)
 		return
 	}
+	startTime := time.Now()
 	atomic.AddUint64(&s.Requests, 1)
 	atomic.AddUint64(&s.MgetRequests, 1)
 
@@ -103,6 +115,7 @@ func (s *httpServer) mgetHandler(w http.ResponseWriter, req *http.Request) {
 	} else {
 		atomic.AddUint64(&s.MgetHits, 1)
 	}
+	s.MgetMetrics.Status(startTime)
 }
 
 func (s *httpServer) reloadHandler(w http.ResponseWriter, req *http.Request) {
@@ -112,25 +125,41 @@ func (s *httpServer) reloadHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 type statsResponse struct {
-	Requests     uint64 `json:"total_requests"`
-	GetRequests  uint64 `json:"get_requests"`
-	GetHits      uint64 `json:"get_hits"`
-	GetMisses    uint64 `json:"get_misses"`
-	MgetRequests uint64 `json:"mget_requests"`
-	MgetHits     uint64 `json:"mget_hits"`
-	MgetMisses   uint64 `json:"mget_misses"`
+	Requests     uint64        `json:"total_requests"`
+	GetRequests  uint64        `json:"get_requests"`
+	GetHits      uint64        `json:"get_hits"`
+	GetMisses    uint64        `json:"get_misses"`
+	GetAvg       time.Duration `json:"get_average_request"` // Microsecond
+	Get95        time.Duration `json:"get_95"`              // Microsecond
+	Get99        time.Duration `json:"get_99"`              // Microsecond
+	MgetRequests uint64        `json:"mget_requests"`
+	MgetHits     uint64        `json:"mget_hits"`
+	MgetMisses   uint64        `json:"mget_misses"`
+	MgetAvg      time.Duration `json:"mget_average_request"` // Microsecond
+	Mget95       time.Duration `json:"mget_95"`              // Microsecond
+	Mget99       time.Duration `json:"mget_99"`              // Microsecond
 }
 
 func (s *httpServer) statsHandler(w http.ResponseWriter, req *http.Request) {
+	getStats := s.GetMetrics.Stats()
+	mgetStats := s.MgetMetrics.Stats()
 	stats := statsResponse{
 		Requests:     atomic.LoadUint64(&s.Requests),
 		GetRequests:  atomic.LoadUint64(&s.GetRequests),
 		GetHits:      atomic.LoadUint64(&s.GetHits),
 		GetMisses:    atomic.LoadUint64(&s.GetMisses),
+		GetAvg:       getStats.Avg / time.Microsecond,
+		Get95:        getStats.P95 / time.Microsecond,
+		Get99:        getStats.P99 / time.Microsecond,
 		MgetRequests: atomic.LoadUint64(&s.MgetRequests),
 		MgetHits:     atomic.LoadUint64(&s.MgetHits),
 		MgetMisses:   atomic.LoadUint64(&s.MgetMisses),
+		MgetAvg:      mgetStats.Avg / time.Microsecond,
+		Mget95:       mgetStats.P95 / time.Microsecond,
+		Mget99:       mgetStats.P99 / time.Microsecond,
 	}
+	// evbuffer_add_printf(evb, "\"total_seeks\": %"PRIu64",", total_seeks);
+
 	response, err := json.Marshal(stats)
 	if err != nil {
 		log.Printf("%s", err)
@@ -142,17 +171,5 @@ func (s *httpServer) statsHandler(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Length", strconv.Itoa(len(response)))
 	w.WriteHeader(200)
 	w.Write(response)
-
-	// for (i = 0; i < st->callback_count; i++) {
-	//     evbuffer_add_printf(evb, "\"%s_95\": %"PRIu64",", st->stats_labels[i], st->ninety_five_percents[i]);
-	//     evbuffer_add_printf(evb, "\"%s_average_request\": %"PRIu64",", st->stats_labels[i], st->average_requests[i]);
-	//     evbuffer_add_printf(evb, "\"%s_requests\": %"PRIu64",", st->stats_labels[i], st->stats_counts[i]);
-	// }
-	// evbuffer_add_printf(evb, "\"get_hits\": %"PRIu64",", get_hits);
-	// evbuffer_add_printf(evb, "\"get_misses\": %"PRIu64",", get_misses);
-	// evbuffer_add_printf(evb, "\"fwmatch_hits\": %"PRIu64",", fwmatch_hits);
-	// evbuffer_add_printf(evb, "\"fwmatch_misses\": %"PRIu64",", fwmatch_misses);
-	// evbuffer_add_printf(evb, "\"total_seeks\": %"PRIu64",", total_seeks);
-	// evbuffer_add_printf(evb, "\"total_requests\": %"PRIu64, st->requests);
 
 }
